b569169ec76dab8e7f875b6e88a9e6f8c8b03785
JakubCiesko <jakub.ciesko@gmail.com>
2025-05-08 00:50:24 +0200

    final index.html form; added timestamp to index

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
 
 
@@ -12,11 +11,15 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def root(request: Request):
+async def index(request: Request):
     now = datetime.now()
     return templates.TemplateResponse(
         request=request, name="index.html", context={"now": now}
     )
+
+@app.get("/products", response_class=JSONResponse)
+async def get_products():
+    return JSONResponse({"message": "OK"}, 200)
 
 
 
